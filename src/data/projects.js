088384be09c441548c2841d6d69c1830994c48d4
Ramiz Shahin <119672563+RamizShahin@users.noreply.github.com>
2025-07-31 00:02:55 +0300

    new updates

--- src/data/projects.js
+++ src/data/projects.js
@@ -8,5 +8,14 @@ const projectsData = [
     githubLink: "https://github.com/RamizShahin/regression-management",
     liveLink: "http://rmportal.duckdns.org/",
   },
+  {
+    title: "Expense Tracking",
+    description:
+      "A full-stack application for tracking expenses/incomes, with charts for better analysis.",
+    image: "/My-Portfolio/images/projects/expense-tracking.jpg",
+    technologies: ["React", "Node.js", "Express", "MongoDB"],
+    githubLink: "https://github.com/RamizShahin/Expense-Tracker",
+    liveLink: "https://expense-tracker-silk-two-37.vercel.app/",
+  },
 ];
 export default projectsData;
